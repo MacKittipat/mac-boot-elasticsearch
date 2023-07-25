f127f3f23772b5b23d12b7820a25e4bb71b089fa
Mac <mac.kittipat@gmail.com>
2023-07-25 21:34:13 +0700

    Init project

---
+++ src/main/java/com/mackittipat/macbootelasticsearch/MacBootElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacBootElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacBootElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mackittipat/macbootelasticsearch/MacBootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macbootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacBootElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
